83e7bf9e2151badd4c33c27b1d35e9139afe3884
enoch <henokzjohn@gmail.com>
2025-09-09 11:42:50 +0300

    feat: implement randomColor() and return button
    Description: randomColor() gives the pen-like trail random colors and return button works just fine

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
-// const { createElement } = require("react");
-
 const body = document.querySelector('body');
 const inputDiv = document.createElement('div');
+inputDiv.className = "inputDiv";
 body.appendChild(inputDiv);
 
 const text = document.createElement('h1');
@@ -13,49 +12,108 @@ defaultBtn.textContent = "Default";
 confirmBtn.textContent = "Confirm";
 inputDiv.append(text, input, confirmBtn, defaultBtn);
 
-// function restorePage () {
-//     const text = document.createElement('h1');
-//     text.textContent = "Enter the number of grids you want";
-//     const input = document.createElement('input');
-//     const defaultBtn = document.createElement('button');
-//     const confirmBtn = document.createElement('button');
-//     defaultBtn.textContent = "Default";
-//     confirmBtn.textContent = "Confirm";
-//     inputDiv.append(text, input, confirmBtn, defaultBtn);
-// }
-
-// restorePage();
-
-function clearPage () {
-    body.textContent = "";
+function init () {
+    text.textContent = "Enter the number of grids you want";
+    defaultBtn.textContent = "Default";
+    confirmBtn.textContent = "Confirm";
+    // console.log(document);
 }
 
-defaultBtn.addEventListener('click', () => {
+function restorePage () {
     clearPage();
+
+    if (!body.contains(inputDiv)) {
+        body.append(inputDiv);
+    }
+    init();
+}
+
+function clearPage () {
+    // body.textContent = "";
+    const mainDiv = document.querySelector('.mainDiv');
+    if (mainDiv) {
+        mainDiv.remove();
+    }
+}
+
+function removeInputPage() {
+    const inputDiv = document.querySelector('.inputDiv');
+    if (inputDiv && body.contains(inputDiv)) {
+        inputDiv.remove();
+    }
+
+}
+
+function divCreator (numOfDivs) {
     const mainDiv = document.createElement('div');
     mainDiv.className = 'mainDiv';
 
-    for (let i = 0; i < 16; i++) {
+    const etchSketchContainer = document.createElement('div');
+    etchSketchContainer.className = "etchSketchContainer";
+
+    const returnBtn = document.createElement('button'); 
+    returnBtn.addEventListener('click', restorePage);
+    returnBtn.textContent = "Return"
+    mainDiv.appendChild(returnBtn);
+
+    for (let i = 0; i < numOfDivs; i++) {
         let innerDiv = document.createElement('div');
         innerDiv.className = 'inner';
         // innerDiv.textContent = "1";
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < numOfDivs; j++) {
             const div = document.createElement('div');
             div.className = 'nested'
             innerDiv.appendChild(div);
         }
-        mainDiv.appendChild(innerDiv);
+        etchSketchContainer.appendChild(innerDiv);
     }
-    console.log(mainDiv);
+    mainDiv.appendChild(etchSketchContainer);
+    // console.log(mainDiv);
     body.append(mainDiv);
 
     const divs = document.querySelectorAll('.nested')
     divs.forEach(div => {
         div.addEventListener('mouseenter', (event) => {
-            event.target.style.backgroundColor = 'aqua';
-        }
-    )
+            if (!event.target.style.backgroundColor) {
+                event.target.style.backgroundColor = randomColor();
+            }
+            // console.log(event.target.style.backgroundColor);
+            // event.target.style.backgroundColor = randomColor();
+            // div.classList = "color-set";
+            // console.log(div.classList);
+            // if (!event.target.style.backgroundColor) {
+            //     // bgColor =  randomColor();
+            //     console.log('yes');
+            //     // return;
+            // }
+            // event.target.style.backgroundColor = randomColor();
+        });
+    });
+};
+
+
+defaultBtn.addEventListener('click', () => {
+    clearPage();
+    removeInputPage();
+    divCreator(12); //12x12
+});
+
+//confirm button
+
+confirmBtn.addEventListener('click', () => {
+    const value = input.value;
+    if (value > 100) {
+        throw new Error("Value should be under 100");
     }
-        );
-    }
-);
+    input.value = "";
+
+    clearPage();
+    removeInputPage();
+    divCreator(value);
+})
+
+//Extras
+
+function randomColor() {
+    return (`rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`);
+}
